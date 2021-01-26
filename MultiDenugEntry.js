cacdceba06bb1c5ec0d1c5dbc8f8bb9d04e4b179
张磊 <634941757@qq.com>
2021-01-26 10:17:02 +0800

    1

--- MultiDenugEntry.js
+++ MultiDenugEntry.js
@@ -3,7 +3,7 @@ import {AppRegistry} from 'react-native';
 
 import {} from 'react-native';
 import App from './App';
-import MiniApp from '../MiniApp';
+import MiniApp from './MiniApp';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
